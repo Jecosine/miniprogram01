9fd00ff6009d3f33bd2c0546381b0d33cd518081
silver <mj_ol@outlook.com>
2019-05-25 02:06:04 +0900

    UPDATE:Add pages

--- pages/passage/test.js
+++ pages/passage/test.js
@@ -1,27 +1,13 @@
-// pages/main/main.js
+// pages/passage/test.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+
   },
-  redirect01(){
-    wx.redirectTo({
-      url: "/pages/passage/test",
-      success: function(res) {},
-      fail: function(res) {},
-      complete: function(res) {},
-    })
-  },
-  redirect02() {
-    wx.redirectTo({
-      url: "/pages/passage/recom",
-      success: function (res) { },
-      fail: function (res) { },
-      complete: function (res) { },
-    })
-  },
+
   /**
    * 生命周期函数--监听页面加载
    */
